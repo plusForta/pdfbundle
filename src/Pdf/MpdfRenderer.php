8d245c30c4492609d64d2ab8a39386e8be320e20
Jan Hentschel <jan.hentschel@plusforta.de>
2020-02-13 16:04:55 +0100

    Refs PF-6160: ERP - API Callbacks - emailBounced implementieren
    removed symfony dependency

--- src/Pdf/MpdfRenderer.php
+++ src/Pdf/MpdfRenderer.php
@@ -5,13 +5,8 @@ namespace PlusForta\PdfBundle\Pdf;
 
 
 use Mpdf\Config\ConfigVariables;
-use Mpdf\HTMLParserMode;
 use Mpdf\Mpdf;
 use Mpdf\Config\FontVariables;
-use Mpdf\Output\Destination;
-use PlusForta\PdfBundle\Pdf\DocumentModel\Document;
-use PlusForta\PdfBundle\Pdf\DocumentModel\Page;
-use PlusForta\PdfBundle\Pdf\DocumentModel\Pagegroup;
 use Psr\Log\LoggerInterface;
 
 class MpdfRenderer implements PdfRendererInterface
@@ -63,65 +58,8 @@ class MpdfRenderer implements PdfRendererInterface
     /** @throws \Mpdf\MpdfException */
     public function render(string $html): string
     {
-        if ($this->directMode) {
-            $this->pdf->WriteHTML($html);
-            return $this->pdf->Output();
-        }
-
-        $document = new Document($html);
-        $this->renderChildren($document->getChildren());
-
         $this->pdf->WriteHTML($html);
-        return $this->pdf->Output(Destination::STRING_RETURN);
+        return $this->pdf->Output();
     }
 
-    /**
-     * @param Page $page
-     * @throws \Mpdf\MpdfException
-     */
-    protected function renderPage(Page $page): void
-    {
-        $style = $page->getStyle();
-        $header = $page->getHeader();
-        $footer = $page->getFooter();
-        $this->logger->debug('renderPage', [
-            'header' => $header,
-            'footer' => $footer,
-            'style' => $style,
-            'content' => $page->getHtml(),
-        ]);
-        if ($style !== null) {
-            $this->pdf->WriteHTML($style, HTMLParserMode::HEADER_CSS);
-        }
-
-        if ($header !== null) {
-            $this->pdf->SetHTMLHeader($header);
-        }
-
-        if ($footer !== null) {
-            $this->pdf->SetHTMLFooter($footer);
-        }
-
-        $this->pdf->addPage();
-        $this->pdf->WriteHTML($page->getHtml());
-    }
-
-    /**
-     * @param array $children
-     * @throws \Mpdf\MpdfException
-     */
-    protected function renderChildren(array $children): void
-    {
-        foreach ($children as $child) {
-            switch (get_class($child)) {
-                case Page::class:
-                    /** @var Page $child */
-                    $this->renderPage($child);
-                    break;
-                case Pagegroup::class:
-                    $this->renderChildren($child->getChildren());
-                    break;
-            }
-        }
-    }
 }
