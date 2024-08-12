8a93d3a7b5b56ea811f41e394234a04dd1133f94
Marc Runkel <marc.runkel@plusforta.de>
2024-08-12 13:19:55 +0200

    Added additional logging to help troubleshoot problem in production

--- src/PlusFortaPdfRenderer.php
+++ src/PlusFortaPdfRenderer.php
@@ -6,12 +6,15 @@ namespace PlusForta\PdfBundle;
 
 use PlusForta\PdfBundle\Html\TemplateEngineInterface;
 use PlusForta\PdfBundle\Pdf\PdfRendererInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 
-class PlusFortaPdfRenderer
+class PlusFortaPdfRenderer implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
     
     /** @var PdfRendererInterface */
-    private $pdf;
+    private $pdfRenderer;
     
     /** @var TemplateEngineInterface */
     private $templateEngine;
@@ -22,9 +25,9 @@ class PlusFortaPdfRenderer
     /** @var string[] */
     private $appendedPdfs = [];
 
-    public function __construct(PdfRendererInterface $pdf, TemplateEngineInterface $templateEngine)
+    public function __construct(PdfRendererInterface $pdfRenderer, TemplateEngineInterface $templateEngine)
     {
-        $this->pdf = $pdf;
+        $this->pdfRenderer = $pdfRenderer;
         $this->templateEngine = $templateEngine;
     }
 
@@ -38,17 +41,36 @@ class PlusFortaPdfRenderer
         $this->appendedPdfs = $files;
     }
 
+    /**
+     * render the PDF from the given template and context
+     */
     public function render(string $templateName, array $context):string
     {
-        $template = $this->renderHtml($templateName, $context);
-        $this->pdf->prependPdf($this->prependedPdfs);
-        $this->pdf->appendPdf($this->appendedPdfs);
-        return $this->pdf->render($template);
+        // get the HTML source for the filled out template
+        $html = $this->renderHtml($templateName, $context);
+        // prepend and append the PDFs
+        $this->pdfRenderer->prependPdf($this->prependedPdfs);
+        $this->pdfRenderer->appendPdf($this->appendedPdfs);
+
+        // render the PDF(with prepended PDFs) and return it
+        return $this->pdfRenderer->render($html);
     }
 
+    /**
+     * create an HTML document from the twig templates
+     **/
     public function renderHtml(string $templateName, array $context): string
     {
-        return $this->templateEngine->render($templateName, $context);
-    }
+        $filledHtml = $this->templateEngine->render($templateName, $context);
+        # find all stylesheets in the HTML
+        # <link rel="stylesheet" href="http://web/build/aarealPdf.css">
+        $matches = [];
+        if (preg_match('/<link rel="stylesheet" href="([^"]+)">/', $filledHtml, $matches)) {
+            if (count($matches) > 1) {
+                $this->logger->info(sprintf('Found stylesheet: %s', $matches[1]));
+            }
+        }
 
+        return $filledHtml;
+    }
 }
